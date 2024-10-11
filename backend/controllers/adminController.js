03b2a95b973e8ae1a158ab33640371c14a31c26e
Papun1111 <gohanmohapatra@gmail.com>
2024-10-11 20:44:39 +0530

    auth admin completed

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
-
+import jwt from "jsonwebtoken"
 const addDoctor = async (req, res) => {
     try {
         const {
@@ -64,5 +64,20 @@ const addDoctor = async (req, res) => {
         res.status(500).json({ success: false, message: error.message });
     }
 };
+//auth api
+const loginAdmin=async(req,res)=>{
+try {
+    const{email,password}=req.body;
+    if (email==process.env.ADMIN_EMAIL && password==process.env.ADMIN_PASSWORD) {
+const token=jwt.sign(email+password,process.env.JWT_SECRET);
+res.json({success:true,token})        
+    }else{
+res.json({success:false,message:"Invalid credentials"})
+    }
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: error.message });  
+}
+}
 
-export { addDoctor };
+export { addDoctor ,loginAdmin};
