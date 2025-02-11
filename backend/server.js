016917cc67e2dc486237c293327f49141d1ef883
rishi99138 <rishimandal99138@gmail.com>
2025-02-11 14:22:21 +0530

    video call feature implemented

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO with CORS
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Add your frontend URLs
+    origin: ["*"], // Add your frontend URLs
     methods: ["GET", "POST"],
   },
 });
